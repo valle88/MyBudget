1715e560d059c9c7faadf8975aa374c8de24b0cc
valle <vallukis@gmail.com>
2022-11-21 13:34:58 +0000

    feat: eliminar componente description porque he pasado ytoda la informacion al modal de componente principal

--- components/Principal.jsx
+++ components/Principal.jsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import { Alert, Modal, StyleSheet, Text, Pressable, View, TextInput, Button } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown';
+import ModalCalendar from './ModalCalendar';
 import Descripcion from "./Descripcion";
 
 const Principal = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const prodTypes = ["Gastos", "Ingreso"];
+
+  const [importe,setImporte]= useState(0);
+  const [concepto, setConcepto]= useState('');
+  const [prodType, setProdType]= useState('category...');
+
+  const changeTextHandler = (value) => {
+      setConcepto(value);
+      
+  }
+  const changeTypeHandler = (value) =>{
+      setProdType(value);
+  }
+  const changeImporteHandler = (value)=>{
+      setImporte(value);
+  }
+
+  const addProductHandle =()=>{
+      concepto.length > 20
+          ? alert('te has escedido de caracteres')
+          : onProductAdd(concepto,prodType,importe);
+  
+      setConcepto('');
+      setImporte(0);
+      setProdType('category...')
+  }
+
+  const isDisabled=()=>{
+      const sanitizedName = concepto.trim();
+
+      if (sanitizedName !== '' && prodType !=='category...') {
+          return false;
+      }
+      return true;
+  };
+
+
+
+
+
   return (
     <View style={styles.centeredView}>
       <Modal
@@ -17,7 +59,48 @@ const Principal = () => {
       >
         <View style={styles.centeredView}>
           
-           <Descripcion/>
+        <View style={styles.border}>
+            <View style={styles.textIn}>
+                <TextInput placeholder='Concepto' 
+                keyboardType='default'
+                onChangeText={changeTextHandler} />
+                <TextInput placeholder='Importe' 
+                keyboardType={'number-pad'} 
+                initValue={importe}
+                onChange={value => changeImporteHandler(value)}/>
+
+            </View>
+            <View style={styles.direccion}>
+                <SelectDropdown
+                    
+                    data={prodTypes}
+                    onSelect={(selectItem) => {
+                        changeTypeHandler(selectItem);
+                    }}
+                    defaultButtonText={'category...'}
+                    buttonTextAfterSelection={() => {
+                        return prodType;
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item;
+                    }}
+                    buttonStyle={styles.dropdownBtnStyle}
+                    buttonTextStyle={styles.dropdownBtnTxtStyle}
+                    dropdownStyle={styles.dropdownDropdownStyle}
+                    rowStyle={styles.dropdownRowStyle}
+                />
+                
+                  <ModalCalendar />
+            </View>
+            
+            <View>
+                <Button style={styles.button1}
+                    title="Añadir"
+                    onPress={addProductHandle}
+                    disabled={isDisabled()}
+                />
+            </View>
+        </View>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
@@ -78,7 +161,26 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 14,
     textAlign: "center"
-  }
+  },
+  direccion: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+},
+
+
+textIn: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    padding:20
+},
+border:{
+    backgroundColor: 'yellow',
+    padding: 10,
+    borderRadius: 10
+},
+button1:{
+    borderRadius:10
+}
 });
 
 export default Principal;
